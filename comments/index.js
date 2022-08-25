aa4a157d2198735b8499d8963c7ab126f78a79f3
Lakour <cnicholaslakour@skillstorm.com>
2022-08-25 12:40:59 -0500

    added comment moderator

--- comments/index.js
+++ comments/index.js
@@ -19,13 +19,13 @@ app.post("/posts/:id/comments", async (req, res, next) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content: content });
+  comments.push({ id: commentId, content: content, status:"pending"});
 
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
-    data: { id: commentId, content: content, postId: req.params.id }
+    data: { id: commentId, content: content, postId: req.params.id, status: "pending"}
   });
 
   res.status(201).send(comments);
--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 
 app.post('/events', async (req, res, next) => {
   const event = req.body;
-  await axios.post("http://localhost:4001/events", event)
-  await axios.post("http://localhost:4000/events", event)
-  await axios.post("http://localhost:4002/events", event)
+  await axios.post("http://localhost:4001/events", event) //comments
+  await axios.post("http://localhost:4000/events", event) //posts
+  await axios.post("http://localhost:4002/events", event) //query
+  await axios.post("http://localhost:4003/events", event) //moderation
 
   res.send({status: "OK"})
 })
