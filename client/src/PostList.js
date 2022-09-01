f787d708fcb036d89d39c43cd4fc48900c905498
Lakour <cnicholaslakour@skillstorm.com>
2022-09-01 10:02:13 -0500

    added skaffold

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -30,7 +30,7 @@ function PostList() {
   });
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.com/posts");
     setPosts(res.data);
   };
 
